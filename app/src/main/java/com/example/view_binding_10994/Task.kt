e6ab3caf0261b5595af37029fada8f9bda8ba1c6
captbay <agespramana9@gmail.com>
2022-09-13 14:14:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10994/Task.kt
@@ -0,0 +1,5 @@
+package com.example.view_binding_10994
+
+data class Task(val chapter : String, val title: String, val details : String) {
+
+}
---
+++ app/src/main/java/com/example/view_binding_10994/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10994
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin impact", "Mencari Primo buat Gacha Mama Yaemiko tercinta / Keging Wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei?", "Buat Adu mekanik Showcase Ayak Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok","Buat pamer ke view showCase yaka Pyro Co + Bennet Co")
+    )
+}
